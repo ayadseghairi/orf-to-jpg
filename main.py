35a139462e9551eb816785866f0f000b34b787bb
Ayad seghairi <85361584+ayadseghairi@users.noreply.github.com>
2022-11-10 16:05:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import glob
 import rawpy
 import imageio
 import os
+
+def progress_bar(progress,total):
+	percent = 100 * (progress / float(total))
+	bar = '#' * int(percent) + '_' * int(100 - percent)
+	print(f"\r|{bar}| {progress:.2f}%",end="\r")
+	
+
+
 path = input('Enter The path : ')
 if os.path.isdir(path):
 	path1 = path+ "/*.ORF"
@@ -17,11 +25,15 @@ if os.path.isdir(path):
 		os.mkdir("ayadseghairi")
 	except FileExistsError:
 		pass
+	i = 1
+	progress_bar(0,len(lst2))
 	for infile in lst2:
+		progress_bar(i,len(lst2))
 		with rawpy.imread(infile) as raw:
 			rgb = raw.postprocess()
 			nouwfilename = "ayadseghairi/" + os.path.basename(infile)[:-4]+'.jpg'
 			imageio.imwrite((str(nouwfilename)), rgb)
+			i+=1
 elif os.path.isfile(path):
 	with rawpy.imread(path) as raw:
 		rgb = raw.postprocess()
