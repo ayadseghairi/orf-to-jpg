38b131484636117097b8f08c5f033eadadcc9870
Ayad seghairi <85361584+ayadseghairi@users.noreply.github.com>
2022-11-13 17:28:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import glob
 import rawpy
 import imageio
 import os
-
-def progress_bar(progress,total):
-	percent = 100 * (progress / float(total))
-	bar = '#' * int(percent) + '_' * int(100 - percent)
-	print(f"\r|{bar}| {progress:.2f}%",end="\r")
-	
-
-
+def progress_bar(progress,total,infile):
+	percent = 50 * (progress / float(total))
+	bar = '█' * int(percent) + ' ' * int(50 - percent)
+	print(f"\r{os.path.basename(infile)}|{bar}| {percent*2:.2f}% |{progress}/{total}",end="\r")
 path = input('Enter The path : ')
 if os.path.isdir(path):
 	path1 = path+ "/*.ORF"
@@ -21,23 +17,31 @@ if os.path.isdir(path):
 			pass
 		else :
 			lst2.append(i)
+	if len(lst2) !=0:
+		try :
+			os.mkdir("ayadseghairi")
+		except FileExistsError:
+			pass
+		i = 1
+		for infile in lst2:
+			progress_bar(i,len(lst2),infile)
+			with rawpy.imread(infile) as raw:
+				rgb = raw.postprocess()
+				nouwfilename = "ayadseghairi/" + os.path.basename(infile)[:-4]+'.jpg'
+				imageio.imwrite((str(nouwfilename)), rgb)	
+				i+=1
+		print('\nDone.')
+	else :
+		print(f"Error no \"*.ORF\" image on \"{path}\".")
+elif os.path.isfile(path):
 	try :
 		os.mkdir("ayadseghairi")
 	except FileExistsError:
 		pass
-	i = 1
-	progress_bar(0,len(lst2))
-	for infile in lst2:
-		progress_bar(i,len(lst2))
-		with rawpy.imread(infile) as raw:
-			rgb = raw.postprocess()
-			nouwfilename = "ayadseghairi/" + os.path.basename(infile)[:-4]+'.jpg'
-			imageio.imwrite((str(nouwfilename)), rgb)
-			i+=1
-elif os.path.isfile(path):
 	with rawpy.imread(path) as raw:
 		rgb = raw.postprocess()
 		nouwfilename = "ayadseghairi/" + os.path.basename(path)[:-4]+'.jpg'
 		imageio.imwrite((str(nouwfilename)), rgb)
+	print('\nDone.')
 else :
 	print(f"Error '{path}' Not found.")
